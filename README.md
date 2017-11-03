2bbf5a7066708b28dc8df9083dc65df4a1bf22ba
infinith4 <infinith4@gmail.com>
2017-11-03 17:16:07 +0900

    Test AddNumber Method

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # UnitTestSample
+
+https://code.msdn.microsoft.com/windowsapps/uniteTestVS2013-fa41a0af
---
+++ UnitTestSample/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestSample;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program p = new Program();
+            int x = 1;
+            int y = 2;
+
+            int expected = 3;
+            //答えとして期待する値 
+
+            int actual = p.AddNumber(x, y);
+            //実際の値 
+
+            Assert.AreEqual(expected, actual);
+            //期待値と実際の値が同一であるかを検証します。同一である場合にはテストが成功し、そうでない場合には失敗を返します。 
+        }
+    }
+}
